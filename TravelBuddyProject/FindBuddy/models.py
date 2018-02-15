53c6ff449f0acdc55efebec123f33b68a4cb7df8
Akshar Joshi <aksharjoshi@Akshars-MacBook-Pro.local>
2018-02-15 14:45:16 -0800

    Initial commit with base creatded

---
+++ TravelBuddyProject/FindBuddy/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Traveller(models.Model):
+    name = models.CharField(max_length=140)
+    number = models.IntegerField()
+    travel_start_date = models.DateTimeField()
+    travel_end_date = models.DateTimeField()
+    country = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
---
+++ TravelBuddyProject/FindBuddy/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def signin(request):
+    return render(request, 'FindBuddy/home.html')
+
+def signup(request):
+    return render(request, 'FindBuddy/basic.html', {'content':['If you would like to contact me, please email me','akshar.joshi91@gmail.com']})
